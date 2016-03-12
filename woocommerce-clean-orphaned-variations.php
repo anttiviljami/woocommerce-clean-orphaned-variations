2c100d4106c5769105952aa89e6dd9a8aeba0286
Antti Kuosmanen <antti@seravo.fi>
2016-03-12 13:20:33 +0200

    Implement the tool with a simple SQL query

--- woocommerce-clean-orphaned-variations.php
+++ woocommerce-clean-orphaned-variations.php
@@ -35,10 +35,31 @@ if (!class_exists('WooCommerce_Clean_Orphaned_Variations')) {
     }
 
     private function __construct() {
+      add_filter( 'admin_init', array( $this, 'handle_woocommerce_tool' ) );
       add_filter( 'woocommerce_debug_tools', array( $this, 'add_woocommerce_tool' ) );
 
       // load textdomain for translations
       add_action( 'plugins_loaded',  array( $this, 'load_our_textdomain' ) );
+    }
+
+    /**
+     * Runs an SQL query in the database, which finds any orphaned variations
+     * and deletes them
+     *
+     * @source: https://gist.github.com/pmgarman/6135967
+     */
+    public function clean_orphaned_variations() {
+      global $wpdb;
+      $sql = "DELETE o FROM $wpdb->posts o
+        LEFT OUTER JOIN $wpdb->posts r
+        ON o.post_parent = r.ID
+        WHERE r.id IS null AND o.post_type = 'product_variation'";
+      $rows = $wpdb->query( $sql );
+
+      if( false !== $rows ) {
+        $this->deleted = $rows;
+        add_action( 'admin_notices', array( $this, 'admin_notice__success' ) );
+      }
     }
 
     /**
@@ -50,13 +71,54 @@ if (!class_exists('WooCommerce_Clean_Orphaned_Variations')) {
         'button'  => __( 'Clean Orphaned Variations', 'woocommerce-clean-orphaned-variations' ),
         'desc'    => __( '<strong class="red">Note:</strong> This option will delete all posts of type product_variation with no existing parent, use with caution!', 'woocommerce-clean-orphaned-variations' ),
       );
-			return $tools;
+      return $tools;
+    }
+
+    /**
+     * Runs the tool
+     *
+     * The tool button, when clicked, will send a GET request to the tab page
+     * along with &action=clean_orphaned_variations
+     */
+    public function handle_woocommerce_tool() {
+      // check that we are on woocommerce system status admin page
+      if( 'wc-status' != $_REQUEST['page'] ) {
+        return;
+      }
+
+      // check that we are on the tools tab
+      if( 'tools' != $_REQUEST['tab'] ) {
+        return;
+      }
+
+      // check permissions
+      if( ! is_user_logged_in() || ! current_user_can('manage_woocommerce') ) {
+        return;
+      }
+
+      if ( ! empty( $_GET['action'] ) && ! empty( $_REQUEST['_wpnonce'] ) && wp_verify_nonce( $_REQUEST['_wpnonce'], 'debug_action' ) ) {
+        if( $_GET['action'] === 'clean_orphaned_variations' ) {
+          $this->clean_orphaned_variations();
+        }
+      }
+    }
+
+    /**
+     * Admin notification after running the tool
+     */
+    public function admin_notice__success( ) {
+      $deleted = $this->deleted;
+    ?>
+<div class="notice notice-success is-dismissible">
+  <p><?php echo wp_sprintf( __('%d orphans were deleted.', 'woocommerce-clean-orphaned-variations'), $deleted ); ?></p>
+</div>
+    <?php
     }
 
     /**
      * Load our textdomain
      */
-    public function load_our_textdomain() {
+    public static function load_our_textdomain() {
       load_plugin_textdomain( 'woocommerce-clean-orphaned-variations', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
     }
   }
